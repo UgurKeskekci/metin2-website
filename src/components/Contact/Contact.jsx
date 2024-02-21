0de3e869a3977939f2b4dca210631d8fda33fc41
Ugur Keskekci <ugur.keskekci@hotmail.com>
2024-02-21 21:43:47 +0300

    footer update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -12,16 +11,10 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:ugur.keskekci@hotmail.com">ugur.keskekci@hotmail.com</a>
+          <a href="mailto:ugur.keskekci@hotmail.com">
+            ugur.keskekci@hotmail.com
+          </a>
         </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/in/u%C4%9Fur-ke%C5%9Fkekci-3518001a2/">linkedin.com/uğur-keşkekci-3518001a2/</a>
-        </li>
-       
       </ul>
     </footer>
   );
